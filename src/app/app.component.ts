d3a320d7cc0de37a7825673285b8e733fed2b717
timhub <yzgogo2011@sina.com>
2017-05-18 19:29:13 +0800

    Initial integration with spring boot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/Rx';
+import { BaseRequestOptions, Http, Headers, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+@Injectable()
 export class AppComponent {
   title = 'app works!';
   name = "";
+  result = "result";
+
+  public serviceURL = "http://localhost:8080/services/hello"
+  console: Console;
+
+  constructor(public http:Http){}
+
+  public click() {
+     return this.http.get(this.serviceURL)
+      .map((response : Response) => {
+        this.result = response.text();
+        return response;
+      })
+      .subscribe((response : Response) => 
+         this.result = response.text());
+
+  }
 }
